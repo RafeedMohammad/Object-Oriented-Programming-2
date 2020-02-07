995848eccaf7b7dc8143710ad3d981a9f9b57c6e
RafeedMohammad <32793478+RafeedMohammad@users.noreply.github.com>
2020-02-08 00:35:44 +0600

    Add files via upload
    Problem Definition: 2. Create a Month class that has a single data member of month number. Include a member method
    that returns the name of the month and another method that returns the number of days in the
    month. The ToString( ) method should return the name and number of days. Write a second class to
    test your Month class. The second class should allow the user to input a month number. Display the
    name of the month associated with the number entered and the number of days in that month. For
    this exercise, use 28 for February. If the user inputs an invalid entry, display an appropriate
    message.

---
+++ Basics/LabTask2/LabTask2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the month number: ");
+            int MonthNumber = Convert.ToInt32(Console.ReadLine());
+
+            Month m = new Month(MonthNumber);
+
+            Console.WriteLine(m);
+        }
+    }
+}
