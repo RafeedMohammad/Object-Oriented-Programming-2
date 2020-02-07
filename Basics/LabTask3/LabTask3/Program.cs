009e12e47b54acddb47f547a403c7928095b4d92
RafeedMohammad <32793478+RafeedMohammad@users.noreply.github.com>
2020-02-08 00:37:57 +0600

    Add files via upload
    Problem Definition: Create an Employee class. Items to include as data members are employee number, name, date of
    birth, date of hire, job description, department, monthly salary and age(calculated from date of
    birth) . Include appropriate constructors and methods. The ToString ( ) method to return all data
    members. Create a second class to test your Employee class.

---
+++ Basics/LabTask3/LabTask3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask3
+{
+    class Employee
+    {
+        public int empNo;
+        public string name { get; set; }
+        public int yearOfHire;
+       // public int currentYear = 2020;
+        public string jobDesc { get; set; }
+        public string dept { get; set; }
+        private decimal sal;
+        //public int age { get; set; }
+
+        public Employee(int sn_no, string nm, int date_of_hire, string job_desc, string department, decimal salary)
+        {
+            empNo = sn_no;
+            name = nm;
+            yearOfHire = date_of_hire;
+            jobDesc = job_desc;
+            dept = department;
+            Salary = salary;
+            
+            
+        }
+
+        public decimal Salary
+        {
+            get{return sal; }
+            set
+            {
+                if(value >= 0)
+                {
+                    sal = value;
+                }
+            }
+        }
+
+       // public int Age
+        //{
+          //  get { return age; }
+            //set
+           // {
+             //   age = currentYear - yearOfHire;
+            //}
+        //}
+
+        public void toString()
+        {
+            Console.WriteLine("++++++++++++++++++++++Employee Information+++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Employee No. " + empNo);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Job Description: {0}\nDepartment: {1}\nSalary: {2}" ,jobDesc, dept, sal);
+            //Console.WriteLine("Age: " + age);
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        }
+
+    }
+}
---
+++ Basics/LabTask3/LabTask3/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask3
+
+{ 
+    class Test
+    {
+        private static DateTime date_of_hire;
+
+        static void Main(string[] args)
+        {
+            //var today = DateTime.Today;
+
+             Employee e1 = new Employee(1, "Rafeed Mohammad", 2015, "Project Manager" ,"Software Department", 200000);
+             Employee e2 = new Employee(2, "Black Sabbath", 1882, "Managing Director", "Human Resource Management Dept.", 200000);
+             e1.toString();
+             e2.toString();
+
+    }
+   }
+}
+
