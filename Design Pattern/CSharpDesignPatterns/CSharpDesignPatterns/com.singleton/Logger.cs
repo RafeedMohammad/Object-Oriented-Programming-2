fa56b31759c971cee4fc63684ebc1f50d6850068
RafeedMohammad <32793478+RafeedMohammad@users.noreply.github.com>
2020-02-08 00:42:31 +0600

    Add files via upload
    Singleton Pattern

---
+++ Design Pattern/CSharpDesignPatterns/CSharpDesignPatterns/com.singleton/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns.com.singleton
+{
+    class Logger
+    {
+        private static Logger logger;
+        private Logger()   //private: no other classes can't create any object of the class Logger!
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if(logger == null)
+            {
+                logger = new Logger();
+            }
+
+            return logger;
+
+        }
+    }
+}
---
+++ Design Pattern/CSharpDesignPatterns/CSharpDesignPatterns/com.singleton/TestLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns.com.singleton
+{
+    class TestLogger
+    {
+        static void Main(string[] args)
+        {
+            //Logger obj1 = new Logger();
+            //Logger obj2 = new Logger();
+            Logger obj1 = Logger.GetInstance();
+            Logger obj2 = Logger.GetInstance();
+
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+
+        }
+    }
+}
